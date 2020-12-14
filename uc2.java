0632d520da99993106315af776c6aed61b8c77c8
leena1234gitgit <leenakeni1234@gmail.com>
2020-12-14 16:40:33 +0530

    first commit

--- uc2.java
+++ uc2.java
@@ -1,5 +1,15 @@
-import java.util.*;
- public static void main(String[] args) {
+import java util.*;
+public static void main(String[] args) {
+  char[] board = createBoard();
+}
+
+private static char[] createBoard() {
+   char[] board = new char[10];
+for(int i = 0; i < board.length ; i++?){
+  board[i] = ' ';
+}
+    return board;
+}
 
 public void printBoard() {
    for (int a = 0; a < 9; a++) {
@@ -10,7 +20,7 @@ public void printBoard() {
 }
   else {
  currentPlayer == 'X';
- System.out.println("choose player or computer");
+ System.out.println("choose player : ");
         printBoard();
         System.out.println("X will play first:");
 }
