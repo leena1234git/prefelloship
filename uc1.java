6a07de098346b1f9ba2244b5a9cdd4cf6f5d255a
leena1234gitgit <leenakeni1234@gmail.com>
2020-12-14 16:15:04 +0530

    first commit

---
+++ uc1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public static void main(String[] args) {
+  char[] board = createBoard();
+}
+
+private static char[] createBoard() {
+   char[] board = new char[10];
+for(int i = 0; i < board.length ; i++?){
+  board[i] = ' ';
+}
+    return board;
+}
+
