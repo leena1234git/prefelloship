631e27da53571dec0aa25a23293eb2e22535a653
leena1234gitgit <leenakeni1234@gmail.com>
2020-12-14 16:30:24 +0530

    first commit

--- uc2.java
+++ uc2.java
@@ -2,12 +2,15 @@ import java.util.*;
  public static void main(String[] args) {
 
 public void printBoard() {
-   for (int a = 0; a < 9; a++) { 
-            board[a] = String.valueOf(a + 1); 
-        } 
-  
-        System.out.println("Welcome to 3x3 Tic Tac Toe."); 
+   for (int a = 0; a < 9; a++) {
+
+}
+  if (currentPlayer == 'X')
+      (currentPlayer == '0');
+}
+  else {
+ currentPlayer == 'X';
+ System.out.println("choose player or computer");
         printBoard();
-        System.out.println(
-            "X will play first. Enter a slot number to place X in:"); 
+        System.out.println("X will play first:");
 }
